c7d170999d0678f4a86b55e31fa38448159a8e86
dndphu <dndphu@gmail.com>
2024-02-21 14:01:34 +0700

    init: project

---
+++ src/user/user.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+@Module({
+  controllers: [UserController],
+  providers: [],
+})
+export class UserModule {}
